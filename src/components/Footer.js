2c8758ead51f7b7bcccbf8040a705f47118056a0
moKidesign <73155759+moKidesign@users.noreply.github.com>
2020-11-21 01:44:24 -0800

    footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,15 +4,15 @@ import './Footer.css'
 export default () => (
   <div>
     <h2 className="taCenter">
-      Follow us{' '}
-      <a href="https://instagram.com/thrivegoldcoast/">@thrivegoldcoast</a>
+      CALL US FOR A FREE ESTIMATE{' '}
+      <a href="tel://+12193159477">(219) 315-9477</a>
     </h2>
     <br />
     <footer className="footer">
       <div className="container taCenter">
         <span>
           © Copyright {new Date().getFullYear()} All rights reserved. Crafted by{' '}
-          <a href="https://thriveweb.com.au/">Thrive</a>.
+          <a href="#">moKi™</a>.
         </span>
       </div>
     </footer>
