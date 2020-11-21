5053aa66e35e1527d576d25d4acb75ff5beacb98
moKidesign <73155759+moKidesign@users.noreply.github.com>
2020-11-21 02:02:44 -0800

    title

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,8 +2,8 @@ const postcssPresetEnv = require('postcss-preset-env')
 
 module.exports = {
   siteMetadata: {
-    title: 'Yellowcake',
-    siteUrl: 'https://yellowcake.netlify.com'
+    title: 'Hand Man Services',
+    siteUrl: 'https://www.jmillerservices.com'
   },
   plugins: [
     'gatsby-plugin-react-helmet',
